bbfc7122b73256e59c8184091bdf08176010ff13
가동재 <masterforce999@naver.com>
2025-01-16 08:41:53 +0900

    css 정리

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,32 +1,42 @@
 import Link from "next/link";
 
 export default async function Footer() {
+  const footerStyle =
+    "w-[100dvw]-[45px] flex tablet:h-[296px] justify-between bg-black tablet:px-[60px] mobile:px-[8vw] py-[30px] text-white mobile:flex-col tablet:flex-row mobile:h-auto mobile:gap-y-6 tablet:gap-y-0";
+  const pStyle = "text-body02r text-gray-300";
+  const linkStyle = "text-body02r underline underline-offset-4";
+
   return (
-    <footer className="w-[100dvw]-[45px] flex h-48 justify-between bg-[#000] pt-[30px]">
-      <div className="flex w-[75dvw] flex-col gap-3 pl-[60px]">
-        <div className="text-[28px] font-normal text-white">FURNITURE</div>
-        <div className="whitespace-pre-wrap text-[13px] leading-[18px] text-[#DDD]">
-          {`서울특별시 우자 산업단지 유튜브 99, 울트라사옥  대표이사: 홍길동\n사업자등록번호 : 123-45-67890 / 통신판매업신고 : 9999-12345호\n이메일: madia_madia@naver.com`}
-        </div>
+    <footer className={footerStyle}>
+      <div className="flex flex-col mobile:w-full tablet:w-auto tablet:flex-1 desktop:flex-none">
+        <h3 className="mb-[50px]">로고</h3>
+        <address className="not-italic">
+          <ul className={`flex flex-col gap-y-2 ${pStyle}`}>
+            <li>도로명 주소: 주소를 쓸거임</li>
+            <li>지번주소: 주소를 쓸거임</li>
+            <li>이메일: </li>
+          </ul>
+        </address>
       </div>
-      <div className="flex w-[25dvw] flex-col gap-6 bg-[#000]">
-        <div>
-          <div className="mb-2 text-[24px] font-bold text-white">1588-1234</div>
-          <div className="mb-2 text-[13px] font-medium text-[#DDD]">
-            오전 9시 ~ 오후 6시(토요일, 공휴일 휴무)
-          </div>
-          <div className="mb-2 flex gap-[20px] text-[#DDD]">
-            <Link href="" className="underline decoration-1">
-              1:1문의하기
+      <hr className="h-[1px] bg-white mobile:block tablet:hidden" />
+      <div className="mobile:w-full tablet:w-auto tablet:flex-1 desktop:flex-none">
+        <h3 className="text-heading03b">031-311-1111</h3>
+        <p className={pStyle}>운영시간: 10:00 ~ 18:00</p>
+        <ul className="mb-12 mt-4 flex gap-x-4">
+          <li>
+            <Link href="/" className={linkStyle}>
+              인천교구청
             </Link>
-            <Link href="" className="underline decoration-1">
-              자주 묻는 질문
+          </li>
+          <li>
+            <Link href="/" className={linkStyle}>
+              인천교구 청소년 사목국
             </Link>
-          </div>
-        </div>
-        <div className="text-[13px] font-normal text-[#DDD]">
-          Copyright ⓒ MADIA DESIGNER ALL RIGHTS RESERVED.
-        </div>
+          </li>
+        </ul>
+        <p className={pStyle}>
+          Copyright ⓒ 인천교구 은계성당 All rights reserved
+        </p>
       </div>
     </footer>
   );
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -28,7 +28,7 @@ export default function Menu({
     };
   }, []);
 
-  const menuStyle = `duration-800 fixed right-0 top-0 z-[3000] h-[100dvh] bg-white transition-transform mobile:w-[100dvw] tablet:w-[80dvw] desktop:hidden ${isTriggerAnimation ? "translate-x-0" : "mobile:translate-x-[100dvw] tablet:translate-x-[80dvw]"}`;
+  const menuStyle = `duration-800 fixed right-0 top-0 z-[3000] h-[100dvh] bg-white transition-transform mobile:w-[100dvw] tablet:w-[80dvw] desktop:hidden ${isTriggerAnimation ? "translate-x-0" : "mobile:translate-x-[100dvw] tablet:translate-x-[80dvw]"} overflow-y-auto`;
   const menuLiStyle = "block h-12 py-2.5 text-body01m text-gray-500";
   const bgStyle =
     "fixed left-0 top-0 z-[2001] h-[100dvh] w-[100dvw] cursor-pointer bg-dimmedEffectBlack70D desktop:hidden";
@@ -40,34 +40,36 @@ export default function Menu({
         className={menuStyle}
         onTransitionEnd={handleTransitionEnd}
       >
-        <div className="flex h-14 items-center px-3">
-          <IconButton
-            icons={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M6.29289 6.29289C6.68342 5.90237 7.31658 5.90237 7.70711 6.29289L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L6.29289 7.70711C5.90237 7.31658 5.90237 6.68342 6.29289 6.29289Z"
-                  fill="#111111"
-                />
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M17.7071 6.29289C18.0976 6.68342 18.0976 7.31658 17.7071 7.70711L7.70711 17.7071C7.31658 18.0976 6.68342 18.0976 6.29289 17.7071C5.90237 17.3166 5.90237 16.6834 6.29289 16.2929L16.2929 6.29289C16.6834 5.90237 17.3166 5.90237 17.7071 6.29289Z"
-                  fill="#111111"
-                />
-              </svg>
-            }
-            onClick={onClose}
-          />
+        <div className="sticky top-0 z-10 flex w-full flex-col">
+          <div className="flex h-14 items-center bg-white px-3">
+            <IconButton
+              icons={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M6.29289 6.29289C6.68342 5.90237 7.31658 5.90237 7.70711 6.29289L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L6.29289 7.70711C5.90237 7.31658 5.90237 6.68342 6.29289 6.29289Z"
+                    fill="#111111"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M17.7071 6.29289C18.0976 6.68342 18.0976 7.31658 17.7071 7.70711L7.70711 17.7071C7.31658 18.0976 6.68342 18.0976 6.29289 17.7071C5.90237 17.3166 5.90237 16.6834 6.29289 16.2929L16.2929 6.29289C16.6834 5.90237 17.3166 5.90237 17.7071 6.29289Z"
+                    fill="#111111"
+                  />
+                </svg>
+              }
+              onClick={onClose}
+            />
+          </div>
+          <hr className="h-1.5 w-full border-none bg-gray-100" />
         </div>
-        <hr className="h-1.5 w-full border-none bg-gray-100" />
         <ul>
           <li>
             <Accordion title="테스트">
