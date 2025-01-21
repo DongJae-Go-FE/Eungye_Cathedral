279d92d05b94267aeccbb1e7dd3ce4d32be03834
Kurman11 <skedyd45@naver.com>
2025-01-21 22:53:45 +0900

    feature/card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return (
+    <figure>
+      <div className="w-full h-96 bg-black"></div>
+      <figcaption>
+        <h4 className="w-4/5 truncate text-heading03m font-bold ">제목제목제목제목제목제목제목제목제목제목제목</h4>
+        <p className="text-body01r">날자</p>
+      </figcaption>
+    </figure>
+  )
+}
