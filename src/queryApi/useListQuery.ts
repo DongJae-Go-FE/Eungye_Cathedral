4c2ba3a8b6be6147386df1f58d2dfa14d94e2ea7
가동재 <masterforce999@naver.com>
2025-02-03 16:16:31 +0900

    react query setting

--- src/queryApi/useListQuery.ts
+++ src/queryApi/useListQuery.ts
@@ -7,12 +7,18 @@ export function useNews({ page = "1", limit = "10", search }: ListParamsType) {
   return useQuery(queryOptions.news({ page, limit, search }));
 }
 
-export function useNotices({ page = "1", limit = "10", search }: ListParamsType) {
-    return useQuery(queryOptions.notices({ page, limit, search }));
-  }
+export function useNotices({
+  page = "1",
+  limit = "10",
+  search,
+}: ListParamsType) {
+  return useQuery(queryOptions.notices({ page, limit, search }));
+}
 
-  
-  export function useWeeklys({ page = "1", limit = "10", search }: ListParamsType) {
-    return useQuery(queryOptions.weeklys({ page, limit, search }));
-  }
-  
+export function useWeeklys({
+  page = "1",
+  limit = "10",
+  search,
+}: ListParamsType) {
+  return useQuery(queryOptions.weeklys({ page, limit, search }));
+}
