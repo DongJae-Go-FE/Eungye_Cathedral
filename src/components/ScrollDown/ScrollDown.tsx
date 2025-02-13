9a845b6d29c1a9ccf316349a2a7d37f407029956
가동재 <masterforce999@naver.com>
2025-02-13 16:58:50 +0900

    scrollDown style update

--- src/components/ScrollDown/ScrollDown.tsx
+++ src/components/ScrollDown/ScrollDown.tsx
@@ -43,9 +43,24 @@ export default function ScrollDown({
       className={`scroll-down mobile:bottom-2 tablet:bottom-8 absolute left-1/2 -translate-x-1/2 p-0 ${hide ? "invisible opacity-0" : ""}`}
     >
       <div
-        className={`animate-bounce ${color === "black" ? "text-black" : "text-white"}`}
+        className={`animate-bounce text-heading04r ${color === "black" ? "text-black" : "text-white"}`}
       >
         Scroll Down
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          viewBox="0 0 14 14"
+          fill="none"
+          className="m-auto"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M3.37917 4.83736C3.60697 4.60955 3.97632 4.60955 4.20413 4.83736L7.29165 7.92488L10.3792 4.83736C10.607 4.60955 10.9763 4.60955 11.2041 4.83736C11.4319 5.06516 11.4319 5.43451 11.2041 5.66232L7.70413 9.16232C7.47632 9.39012 7.10697 9.39012 6.87917 9.16232L3.37917 5.66232C3.15136 5.43451 3.15136 5.06516 3.37917 4.83736Z"
+            fill="#fff"
+          />
+        </svg>
       </div>
     </div>
   );
