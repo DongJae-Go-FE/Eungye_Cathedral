fa75362007facafc297b8b9c0996f2bb0539bd9e
가동재 <masterforce999@naver.com>
2025-02-17 00:25:40 +0900

    초등부 컴포넌트 나누고 있음

--- src/components/SchoolCard/SchoolCard04/SchoolCard04.tsx
+++ src/components/SchoolCard/SchoolCard04/SchoolCard04.tsx
@@ -3,7 +3,7 @@ export default function SchoolCard04() {
     <div className="group tablet:mt-[200px] tablet:max-w-[calc(57%-40px)] tablet:text-right relative flex w-full flex-col gap-y-8">
       <div className="mobile:aspect-square tablet:aspect-[634/412] relative overflow-hidden rounded-[20px]">
         <div className="absolute top-0 left-0 h-full w-full scale-100 rounded-[20px] bg-[url('/bg4.png')] bg-cover object-contain object-center transition-all duration-[400ms] group-hover:scale-125 group-hover:opacity-80 group-hover:blur-[22px]" />
-        <div className="tablet:right-8 tablet:bottom-16 absolute flex flex-col items-end gap-y-4 text-white opacity-0 transition-opacity duration-[400ms] group-hover:opacity-100">
+        <div className="mobile:school-card-mobile-center tablet:w-auto tablet:h-auto tablet:justify-start tablet:right-8 tablet:bottom-16 tablet:items-end absolute flex flex-col gap-y-4 text-white opacity-0 transition-opacity duration-[400ms] group-hover:opacity-100">
           <h6 className="text-2xl font-black tracking-widest">제목4입니다.</h6>
           <p className="w-3/4 text-[18px] whitespace-pre-wrap">
             내용입니다 내용입니다 내용입니다 내용입니다 내용입니다 내용입니다.
