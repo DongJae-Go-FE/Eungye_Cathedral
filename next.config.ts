f7d63a6542adf66852942d137e3d448856db56a5
가동재 <masterforce999@naver.com>
2025-02-23 21:53:52 +0900

    보안처리

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
